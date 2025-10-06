427fa6dc9c148604c7ba85faa0a8cf449b8da222
Yash Joshi <thenameisyash.018@gmail.com>
2025-10-07 03:07:12 +0530

    Delete AtmApp.java

--- AtmApp.java
+++
@@ -1,7 +0,0 @@
-public class AtmApp {
-    public static void main(String[] args) {
-        AtmOperation operation = new BankAccount(10000.00);
-        Atm atm = new Atm(operation);
-        atm.Start();
-    }
-}
